52da0598a4239ecb9849c1974218b06fbe6cbdfb
sakele <107649303+sakele@users.noreply.github.com>
2023-10-24 16:53:33 -0700

    Enable all tests and complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let returnString = '';
+    if (num < 0) {
+        return 'ERROR';
+    }
+    for (let i = num; i > 0; i--) {
+        returnString += string;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
