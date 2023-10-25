ecb1cc0af92d7a0ce245bf39d44b025dcf56ea81
sakele <107649303+sakele@users.noreply.github.com>
2023-10-24 17:57:12 -0700

    Enable all tests and complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof(a) !== 'number' || typeof(b) !== 'number' || a < 0 || b < 0) {
+        return 'ERROR';
+    }
+    let sum = 0;
+    let i = min = Math.min(a, b);
+    let max = Math.max(a, b);
+    while (i <= max) {
+        sum += i;
+        i++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
