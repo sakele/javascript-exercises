bab6380813bd18dedf9e21ced1fd5fddee1e7ab9
sakele <107649303+sakele@users.noreply.github.com>
2023-11-09 17:04:27 -0800

    Enable all tests and complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+    count = Number(count);
+    if (count < 0) return 'OOPS' ;
+    if (count === 0) return 0;
+    if (count === 1) return 1;
+    
+    let minusOne = 1;
+    let minusTwo = 0;
+    let current = 0;
+
+    for (let i = 2; i <= count; i++) {
+        current = minusOne + minusTwo;
+        minusTwo = minusOne;
+        minusOne = current;
+    }
+    return current;
 
 };
 
