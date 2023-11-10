4336eec72aaf6f3a8b299e5befb6d5d04e000bb0
sakele <107649303+sakele@users.noreply.github.com>
2023-11-09 17:33:21 -0800

    Enable all tests and complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    function getAge(birth, death) {
+        if (!death) {
+            death = new Date().getFullYear();
+        }
+        return death - birth;
+    }
+    return arr.reduce(function(oldest, person) {
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let currentAge = getAge(person.yearOfBirth, person.yearOfDeath)
+        if (oldestAge < currentAge) {
+            oldest = person;
+        }
+        return oldest;
+    });
 };
 
 // Do not edit below this line
